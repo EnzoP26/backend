0f647b7ccce7378d9071a13190ac9319b39fb3da
Enzo Palermo <epalermo@utbm.fr>
2019-10-04 11:46:21 +0200

    ajout de l'incrémentation

--- test/models/counter_test.rb
+++ test/models/counter_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CounterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should increment the value" do
+
+    counter = Counter.new
+    counter.value = 0
+
+    counter.increment!
+
+    assert counter.value == 1
+
+  end
 end
