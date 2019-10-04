e69bd15dd7e0399902d20a7139e9246e409a381e
Enzo Palermo <epalermo@utbm.fr>
2019-10-04 11:37:44 +0200

    création scaffold counters

--- db/migrate/20191004093709_create_counters.rb
+++ db/migrate/20191004093709_create_counters.rb
@@ -2,7 +2,7 @@ class CreateCounters < ActiveRecord::Migration[5.2]
   def change
     create_table :counters do |t|
       t.string :name
-      t.int :value
+      t.integer :value
 
       t.timestamps
     end
