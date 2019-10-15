9982ed5308cd45d17d8993adba1d618cf0dcc783
Enzo Palermo <epalermo@utbm.fr>
2019-10-15 18:30:42 +0200

    fix patch counter contro

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -41,7 +41,8 @@ class CountersController < ApplicationController
   # PATCH/PUT /counters/1.json
   def update
     respond_to do |format|
-      if @counter.update(counter_params)
+      @counter.increment!
+      if @counter.save()
         format.html { redirect_to @counter, notice: 'Counter was successfully updated.' }
         format.json { render :show, status: :ok, location: @counter }
       else
